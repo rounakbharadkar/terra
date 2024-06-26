f6ede6c02ca1aecf3271524ed896421467a48e5a
rounakbharadkar <67083091+rounakbharadkar@users.noreply.github.com>
2024-06-26 16:16:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,18 +1,22 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
 import {
     id = "i-052952489d8b0edae"
     to = aws_instance.web1
 
     id = "i-03e6ecb56722d37d0"
-    to = "aws_instance.web2"
+    to = aws_instance.web2
 
     id = "i-097a9e82301aa16f6"
-    to = "aws_instance.web3"
+    to = aws_instance.web3
 
     id = "i-0f4cb6e5b694ae44b"
-    to = "aws_instance.web4"
+    to = aws_instance.web4
 
     id = "i-07626d09b0a6af824"
-    to = "aws_instance.web5"
+    to = aws_instance.web5
 
 
 }
